957b38d60659108c52c6b75c92c8b9742526d8be
amin <amin@shafi.ee>
2019-01-18 15:07:25 +0330

    Initial Commit

---
+++ SimpleUITableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleUITableView
+//
+//  Created by Amin Shafiee on 10/28/1397 AP.
+//  Copyright © 1397 amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
