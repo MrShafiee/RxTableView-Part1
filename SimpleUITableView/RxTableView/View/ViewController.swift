a9a02a3d51f5b97c2174aa19e8ca5d4cac534f52
amin <amin@shafi.ee>
2019-01-20 09:15:55 +0330

    Initial commit

---
+++ SimpleUITableView/RxTableView/View/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  SimpleUITableView
+//
+//  Created by Amin Shafiee on 10/28/1397 AP.
+//  Copyright © 1397 amin. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    private let viewModel = ViewControllerViewModel()
+    private let disposeBag = DisposeBag()
+    var rows = PublishSubject<[ViewControllerModel]>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        
+        setupBinding()
+        viewModel.setDataSoruce()
+    }
+
+    func setupBinding(){
+        
+        viewModel.tableRowsItem
+            .observeOn(MainScheduler.instance)
+            .bind(to: rows)
+            .disposed(by: disposeBag)
+        
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+        
+        rows.bind(to: tableView.rx.items(cellIdentifier: "TableViewCell", cellType: TableViewCell.self)) {  (row,item,cell) in
+            
+            cell.titleLbl.text = item.title
+            cell.subtitleLbl.text = item.subtitle
+            
+        }.disposed(by: disposeBag)
+        
+        
+        tableView.rx.modelSelected(TableViewCell.self).subscribe(onNext: { selectedModel in
+            
+        }).disposed(by: disposeBag)
+        
+    }
+}
+
+extension ViewController:UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
---
+++ SimpleUITableView/RxTableView/ViewModel/ViewControllerViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  ViewControllerViewModel.swift
+//  SimpleUITableView
+//
+//  Created by Amin Shafiee on 10/28/1397 AP.
+//  Copyright © 1397 amin. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class ViewControllerViewModel: NSObject {
+    
+    var tableRowsItem = PublishSubject<[ViewControllerModel]>()
+    var loading = PublishSubject<Bool>()
+    
+    func setDataSoruce() {
+        loading.onNext(true)
+        
+        var items = [ViewControllerModel]()
+        
+        items.append( ViewControllerModel(title:"title number 1", subtitle:"show subtitle number 1") )
+        items.append( ViewControllerModel(title:"title number 2", subtitle:"show subtitle number 2") )
+        items.append( ViewControllerModel(title:"title number 3", subtitle:"show subtitle number 3") )
+        items.append( ViewControllerModel(title:"title number 4", subtitle:"show subtitle number 4") )
+        items.append( ViewControllerModel(title:"title number 5", subtitle:"show subtitle number 5") )
+        items.append( ViewControllerModel(title:"title number 6", subtitle:"show subtitle number 6") )
+        items.append( ViewControllerModel(title:"title number 7", subtitle:"show subtitle number 7") )
+        
+        tableRowsItem.onNext(items)
+        loading.onNext(false)
+    }
+}
